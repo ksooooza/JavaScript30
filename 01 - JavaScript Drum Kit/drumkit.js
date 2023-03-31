9cac692a6d7e9da35d4bfb09e2a7142e9a3f1926
ksooooza <ksoooza@gmail.com>
2023-03-31 11:02:15 -0400

    is this committing?

--- 01 - JavaScript Drum Kit/drumkit.js
+++ 01 - JavaScript Drum Kit/drumkit.js
@@ -1,3 +1,9 @@
+window.addEventListener('keydown', function(e) {const audio = document.querySelector(`audio[data-key="{e.keyCode}"]`);
+audio.currentTime = 0;
+audio.play();
+})
+
+
 const items = document.querySelectorAll('.key');
 
 function clickTrigger() {
